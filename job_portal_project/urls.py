a7f79743b0da3634d18d005a7e7a0a2f77f6ccbb
Preethi V <preethivenugopal17@gmail.com>
2026-03-27 01:13:14 +0530

    added create admin url

--- job_portal_project/urls.py
+++ job_portal_project/urls.py
@@ -19,9 +19,11 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from jobs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',        include('jobs.urls')),
     path('accounts/', include('accounts.urls')),
+    path('create-admin/', views.create_admin),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
